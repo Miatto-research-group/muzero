761bad6047af53521d2e6bca4d966bfb6860821b
Alex <alexandravigneron1@gmail.com>
2021-02-08 17:52:15 +0100

    cleaned code

--- environments/gate_synthesis.py
+++ environments/gate_synthesis.py
@@ -7,16 +7,9 @@ import itertools
 np.random.seed(1954)
 
 class GateSynthesis(Game):
-    num_observations: 3  # number of states to pass to the representation network
-    MDP: True
-    num_players = 1
-    num_gates = 7
-    num_qbits = 2
-    num_actions: num_gates * num_qbits
 
     def __init__(self, target_unitary:np.array, curr_unitary:np.array, max_rwd: int, q1_gates:[np.array], q2_gates:[np.array] ):
         super().__init__()
-        self.state = None #TODO what is it?
         self.curr_unitary = curr_unitary
         self.prev_unitary = curr_unitary
         self.target_unitary = target_unitary
@@ -33,53 +26,50 @@ class GateSynthesis(Game):
         self.neg_reward_history = []
         self.distance_history = []
 
-    @property
-    def turn(self): #no need for one player
-        return 1 #always player 1's turn
-
 
     def dist_to_target(self, unitary):
-        """
-        Computes distance between two unitary operators
-        Natively dist between curr_unitary and target,
-        but possibly between some unitary and target
-        """
+        """Computes distance between curr_unitary and target"""
         return np.power(np.linalg.norm(self.target_unitary - unitary), 2)
 
+
     def qbit_num_to_tensor_index(self, n: int):
+        """Converts a qubit number in the system to its tensor index"""
         return n * 2
 
-    def apply_1q_gate(self, gate:np.array, qbit:int):
-        qb_idx = self.qbit_num_to_tensor_index(qbit)
-        tensored_res = np.tensordot(self.curr_unitary, gate, axes=(qb_idx, 1))
 
-        N = self.curr_unitary.ndim
-        lst = list(range(N))
-        idx = 2 * qbit
-        lst.insert(idx, N - 1)
-        res = np.transpose(tensored_res, lst[:-1])
+    def apply_1q_gate(self, gate:np.array, qbit:int):
+        """Applies a 1qb gate to the current unitary and returns the resulting new unitary (and updates it)"""
+        qb_idx = self.qbit_num_to_tensor_index(qbit)
+        dim = self.curr_unitary.ndim
+        lst = list(range(dim))
+
+        tensored_res = np.tensordot(self.curr_unitary, gate, axes=(qb_idx, 1))
+        lst.insert(qb_idx, dim - 1)
+        res = np.transpose(tensored_res, lst[:-1]) #because of the way numpy handles this
         self.curr_unitary = res
         return res
+
 
     #index qbits as 0,1
     def apply_2q_gate(self, gate: np.array, qbitA: int, qbitB: int):
-        A = 2 * qbitA
-        B = 2 * qbitB
-        tensored_res = np.tensordot(self.curr_unitary, gate, axes=((A, B), (1, 3)))
-        N = self.curr_unitary.ndim
-        lst = list(range(N))
-        if A < B:
-            smaller, bigger = A, B
-            first, second = N - 2, N - 1
+        """Applies a 2qb gate to the current unitary and returns the resulting new unitary (and updates it)"""
+        idx_a = self.qbit_num_to_tensor_index(qbitA)
+        idx_b = self.qbit_num_to_tensor_index(qbitB)
+        dim = self.curr_unitary.ndim
+        lst = list(range(dim))
+
+        tensored_res = np.tensordot(self.curr_unitary, gate, axes=((idx_a, idx_b), (1, 3)))
+        if idx_a < idx_b:
+            smaller, bigger = idx_a, idx_b
+            first, second = dim - 2, dim - 1
         else:
-            smaller, bigger = B, A
-            first, second = N - 1, N - 2
+            smaller, bigger = idx_b, idx_a
+            first, second = dim - 1, dim - 2
         lst.insert(smaller, first)
         lst.insert(bigger, second)
-        res = np.transpose(tensored_res, lst[:-2])
+        res = np.transpose(tensored_res, lst[:-2]) #because of the way numpy handles this
         self.curr_unitary = res
         return res
-
 
 
     def step(self, action):
@@ -90,13 +80,14 @@ class GateSynthesis(Game):
         self.prev_unitary = self.curr_unitary  # save before moving
         if (gate.shape == (2, 2, 2, 2)):  # 2qb
             (qbA, qbB) = qbit
-            self.apply_2q_gate(gate, qbA, qbB)  # 1qb
-        elif (gate.shape == (2, 2)):
+            self.apply_2q_gate(gate, qbA, qbB)
+        elif (gate.shape == (2, 2)): # 1qb
             self.apply_1q_gate(gate, qbit)
         else:
             raise ValueError('Unsupported gate dimension')
         rwd = self.update_data
         return rwd
+
 
     @property
     def actions(self):
@@ -109,14 +100,13 @@ class GateSynthesis(Game):
         q2_actions = list(itertools.product(self.q2_gates, coherent_2q_permutations))
         return q1_actions + q2_actions
 
+
     def select_random_action(self):
+        """Selects a possible action at random"""
         poss_actions = self.actions
         idx = np.random.randint(0,len(poss_actions))
         return poss_actions[idx]
 
-    def select_explicit_action(self, n:int):
-        poss_actions = self.actions
-        return poss_actions[n]
 
     @property
     def reward(self) -> int:
@@ -136,13 +126,16 @@ class GateSynthesis(Game):
     def end(self):
         return self.has_won or self.is_stuck
 
+
     @property
     def has_won(self):
         return np.allclose(self.curr_unitary, self.target_unitary)
 
+
     @property
     def is_stuck(self):  # TODO decide when to consider that this exploration has failed and agent is stuck
         return False
+
 
     @property
     def update_data(self):
